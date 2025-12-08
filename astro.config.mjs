a224983405bc15cb2c54e4d7cb02766937c2b53d
Nicola Lopatriello <nic.lopatriello@gmail.com>
2025-12-08 18:34:40 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+    output: 'static',
 	integrations: [mdx(), sitemap()],
 });
